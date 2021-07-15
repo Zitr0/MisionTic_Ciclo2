4dcb48d1bba8064f5c08d501b6abb08be6b68850
Zitr0 <camiloorvi@gmail.com>
2021-07-15 00:18:28 -0500

    Ciclo 2 - Java

---
+++ Semana1/Ejemplo1/src/ejemplo1/Principal.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo1;
+
+import ejemplo1.ventanas.VentanaPrincipal;
+
+/**
+ *
+ * @author jcortiz
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaPrincipal abrir = new VentanaPrincipal();
+        abrir.setVisible(true);
+           
+        
+    }
+    
+}
---
+++ Semana1/Sesion2ConAnt/src/camiloortiz/ejemplo1/Calculadora.java
@@ -0,0 +1,69 @@
+/*
+*  Clase Principal:
+*  Es por donde arranca el programa
+*  tiene el metodo main()
+ */
+// comentarios de una linea
+
+/*
+
+  Así se hace un 
+  comentario de 
+  varias lineas 
+
+ */
+package camiloortiz.ejemplo1;
+
+/**
+ *
+ * @author jcortiz
+ */
+public class Calculadora {
+
+    public static void main(String ab[]) {
+
+        // alt + shift + f para organizar el código
+        // para imprimir
+        System.out.println("Mi primer código en Java");
+        System.out.println("Otra linea");
+            
+            
+            float n1 = 10.5f;
+            float n2 = 7.2f;
+            float r;
+            
+            r = n1 + n2;
+            
+            System.out.println("La suma de n1 + n2 es igual a " + r);
+                  
+                    
+        // Tipos de datos y variables
+        
+        /*
+            --Caracteres  
+                char ''  o ""  comillas dobles para más de un caracter
+                String
+        
+            --Números
+             Enteros    
+                byte     -128 hasta 127
+                short   -32000 hasta 32000
+                int     -2000000000 hasta 2000000000
+                long
+             Reales
+                float estatura = 1.72F o 1.72f  hasta 4 cifras
+                double PI = 3.14159265359
+                
+            --Booleanos
+                boolean respuesta = true  o  respuesta = false
+        
+            --Objetos
+                
+        */
+        
+        
+        
+        
+    }
+
+}
